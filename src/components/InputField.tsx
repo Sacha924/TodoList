b651ae4244e6fa10b630d022d503dac2ba9665a1
Sacha924 <sacha.simon@edu.devinci.fr>
2023-02-05 20:40:18 +0100

    Can create todo content

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./../styles/inputField.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input type="text" placeholder="Enter a Task" className="input__box" />
+    <form className="input" onSubmit={(e)=>{handleAdd(e);inputRef.current?.blur()}}>
+      <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Enter a Task" className="input__box" />
       <button type="submit" className="input_submit">
         GO
       </button>
