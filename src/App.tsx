f6eeded4d31d2bc441cb2dd1e83677eb60e0d806
Sacha924 <sacha.simon@edu.devinci.fr>
2023-02-05 16:36:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return <div className="App">
+    <span className="heading">Todo App</span>
+  </div>;
+};
+
+export default App;
