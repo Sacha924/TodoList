03349bb872eed960c0570337a06b794097af8af9
Sacha924 <sacha.simon@edu.devinci.fr>
2023-02-05 19:48:38 +0100

    input UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
 const App: React.FC = () => {
-  return <div className="App">
-    <span className="heading">Todo App</span>
-  </div>;
+  return (
+    <div className="App">
+      <span className="heading">Todo App</span>
+      <InputField />
+    </div>
+  );
 };
 
 export default App;
