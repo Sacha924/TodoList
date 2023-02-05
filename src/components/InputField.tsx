6a852d6e330f790ae2629ff4f6a59c96f1b4dc41
Sacha924 <sacha.simon@edu.devinci.fr>
2023-02-06 00:35:20 +0100

    refactoring

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -11,7 +11,13 @@ const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="input" onSubmit={(e)=>{handleAdd(e);inputRef.current?.blur()}}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Enter a Task" className="input__box" />
       <button type="submit" className="input_submit">
         GO
